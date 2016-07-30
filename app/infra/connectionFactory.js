2c42cc1bd0f8be4cc1acf474333bab41bea550c4
Robson Rodrigues <Robson Rodrigues>
2016-07-29 23:41:44 -0300

    Subindo arquivos

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 function createDBConnection(){
     
-    if(!process.env.NODE_ENV || process.env.node === 'dev')
+    if(!process.env.NODE_ENV || process.env.node == 'dev')
     {
         return mysql.createConnection({
             host : 'localhost',
@@ -18,6 +18,15 @@ function createDBConnection(){
             user : 'root',
             password : '948298152Ba',
             database : 'casadocodigo_nodejs_test'
+        });
+    }
+    if(process.env.NODE_ENV == 'production')
+    {
+        return mysql.createConnection({
+            host : 'us-cdbr-iron-east-04.cleardb.net',
+            user : 'b7f0f69f3c7183',
+            password : '1903027c',
+            database : 'heroku_dbbffb9bbbd99e4'
         });
     }
 }
